c87e93aa9d85e551b511827117a50271ce68174c
Jayden Pyles <jpylesbusiness@gmail.com>
2025-04-22 14:06:33 -0500

    feat: add alembic for database migrations

--- start.sh
+++ start.sh
@@ -4,15 +4,13 @@ APP_LEVEL=$APP_LEVEL
 export PATH="/root/.nvm/versions/node/v22.14.0/bin:$PATH"
 
 # Run alembic revisions
-cd /project
-pdm run alembic upgrade head
 
 if [ "$APP_LEVEL" == "PROD" ]; then
     export FRONTEND_COMMAND="npm run start"
-    export BACKEND_COMMAND="pdm run python -m uvicorn src.saas_backend.app:app --host 0.0.0.0 --port 8000"
+    export BACKEND_COMMAND="pdm run alembic upgrade head && python -m uvicorn src.saas_backend.app:app --host 0.0.0.0 --port 8000"
 else
     export FRONTEND_COMMAND="npm run dev"
-    export BACKEND_COMMAND="pdm run python -m uvicorn src.saas_backend.app:app --reload --host 0.0.0.0 --port 8000"
+    export BACKEND_COMMAND="pdm run alembic upgrade head && python -m uvicorn src.saas_backend.app:app --reload --host 0.0.0.0 --port 8000"
 fi
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
